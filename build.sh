b1a42cf009adefdfc27a5ec1b3cf3c42497f1cff
Tinky gorai <109448557+TinkyGorai@users.noreply.github.com>
2025-03-29 21:11:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-if[[$CREATE_SUPERUSER == "true"]]; then
+
+if [[ "$CREATE_SUPERUSER" = "true" ]]; 
+then
     python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
 fi
+

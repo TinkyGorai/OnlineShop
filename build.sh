77ce8d350c1a905e81cc2bbafdf917d19b5b246e
Tinky <tinkygorai9@gmail.com>
2025-03-29 22:29:25 +0530

    added build file

---
+++ build.sh
@@ -0,0 +1,8 @@
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+if [[ "$CREATE_SUPERUSER" = "true" ]]; 
+then
+    python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
+fi
